d1c4770f23f05a2b2009077d25b3ef2e80178725
jacklinenyaga <126550868+jacklinenyaga@users.noreply.github.com>
2025-04-05 08:26:51 +0000

    counter

---
+++ counter/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export default function counter (){
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Current count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
